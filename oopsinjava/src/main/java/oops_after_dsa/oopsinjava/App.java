7a542cd46218c3a8955df18891528b99e849d954
Jeevan Reddy <cj094392@cerner.net>
2024-02-05 21:33:31 +0530

    add maven

---
+++ oopsinjava/src/main/java/oops_after_dsa/oopsinjava/App.java
@@ -0,0 +1,13 @@
+package oops_after_dsa.oopsinjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
